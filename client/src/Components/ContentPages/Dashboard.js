53a8f927fa8bebe94fcc98e4868a18ae9adf5eb4
nguyen315 <hominhnguyen315@gmail.com>
2020-11-12 12:38:49 +0700

    update dropdown sidebar

--- client/src/Components/ContentPages/Dashboard.js
+++ client/src/Components/ContentPages/Dashboard.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 import Navigation from '../Navigation'
 
 function Dashboard() {
+
     return (
         <div>
             <Navigation title="Dashboard" />
--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import { Button, Nav, NavDropdown } from 'react-bootstrap'
 
@@ -12,9 +12,16 @@ import { ImHome } from 'react-icons/im'
 import '../Styles/Sidebar.css'
 
 function Sidebar() {
+    const [show, setShow] = useState(true)
+
+    const dropdownToggle = (isShow, event, {source}) => {
+        if (source === "click") 
+            setShow(isShow)
+    }
+
     return (
         <>
-            <Nav className="sidebar flex-column">
+            <Nav className="sidebar flex-column">                       
 
                 <NavLink className="nav-link" to="/">
                     <img
@@ -25,19 +32,17 @@ function Sidebar() {
                     </img>
                         Hotel De Luna
                     </NavLink>
-                
-                <NavLink className="nav-link sidebar-item mt-5" to="/dashboard"><BsFillGridFill className="mr-4"/>Tình trạng phòng</NavLink>
+
+                <NavLink className="nav-link sidebar-item mt-5" to="/dashboard"><BsFillGridFill className="mr-4" />Tình trạng phòng</NavLink>
                 <NavLink className="nav-link sidebar-item" to="/statistic"><FaChartLine className="mr-4" />Thống kê</NavLink>
-                <NavDropdown title={<span className="mr-2 "><BsInboxesFill className="dropdown-title-icon" /> Quản lý</span>} id="nav-dropdown">
-                    
-                <NavLink className="nav-link sidebar-item" to="/room"><ImHome className="mr-4" />Phòng</NavLink>
-                    
-                <NavLink className="nav-link sidebar-item" to="/service"><MdRoomService className="mr-4" />Dịch Vụ</NavLink>
-                    
-                <NavLink className="nav-link sidebar-item" to="/member"><BsPersonLinesFill className="mr-4" />Nhân viên</NavLink>
+                <NavDropdown show={show} onToggle={dropdownToggle} title={<span className="mr-2 "><BsInboxesFill className="dropdown-title-icon" /> Quản lý</span>} id="nav-dropdown">
+
+                    <NavLink className="nav-link sidebar-item" to="/room"><ImHome className="mr-4" />Phòng</NavLink>
+
+                    <NavLink className="nav-link sidebar-item" to="/service"><MdRoomService className="mr-4" />Dịch Vụ</NavLink>
+
+                    <NavLink className="nav-link sidebar-item" to="/member"><BsPersonLinesFill className="mr-4" />Nhân viên</NavLink>
                 </NavDropdown>
-
-
             </Nav>
         </>
     )
